421d5d8ec11c06e47efddd53ad99b6ac80bfeb01
Chebyr <10753247+chebyr@users.noreply.github.com>
2026-07-05 22:06:02 +0530

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -12,9 +12,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 # Register the AOS Blueprint
 # bp = aos_app.get_blueprint()
-# app.register_blueprint(bp)
+# app.register_functions(bp)
 
-app.register_blueprint(auth_blueprint)
-app.register_blueprint(boardroom_blueprint)
-app.register_blueprint(seo_blueprint)
-app.register_blueprint(health_blueprint)
+app.register_functions(auth_blueprint)
+app.register_functions(boardroom_blueprint)
+app.register_functions(seo_blueprint)
+app.register_functions(health_blueprint)
